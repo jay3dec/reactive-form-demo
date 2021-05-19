8f49ab9ebeadccddd846b11530a8e0913b2e02a3
jay3dec <jay3dec@gmail.com>
2021-05-19 23:46:36 +0530

    added validation

--- form-app/src/app/root/root.component.ts
+++ form-app/src/app/root/root.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -8,15 +8,48 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./root.component.css']
 })
 export class RootComponent implements OnInit {
-  userForm;
+  
+  userForm : FormGroup;
+  formSubmitted : boolean = false;
+  emailIds = ["jay3dec@gmail.com","samhogn@gmail.com","abc@gmail.com"];
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
     this.userForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
-      lastName: ['',[Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
-      email: ['', [Validators.required, Validators.email]]
-    });
+      firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$'), Validators.maxLength(10)]],
+      lastName: ['',[Validators.required, Validators.pattern('^[a-zA-Z]+$'), Validators.maxLength(10)]],
+      email: ['', [Validators.required, Validators.email, this.duplicateEmailValidator.bind(this)]],
+      employed : [''],
+      companyName: ['']
+    }, {validator : this.companyEmailValidation});
+
+    this.handleValueChange();
+    
+  }
+
+  handleValueChange(){
+    this.userForm.get('employed').valueChanges.subscribe(response => {
+      console.log('check response is ', response);
+      if(response == true){
+        this.userForm.get('companyName').setValidators(Validators.required);
+      } else {
+        this.userForm.get('companyName').clearValidators();
+      }
+      this.userForm.get('companyName').updateValueAndValidity();
+    })
+  }
+
+  companyEmailValidation(formGroup : FormGroup){
+    const email = formGroup.get('email').value;
+    const company = formGroup.get('companyName').value;
+    console.log('email ', email, ' company ', company);
+    if(email && email.length > 0){
+      let comName = email.split("@")[1].split(".")[0];
+      if(company == comName) return null;
+    }
+    return {
+      companyEmailError : true
+    }
   }
 
   onSubmit(){
@@ -27,8 +60,20 @@ export class RootComponent implements OnInit {
         console.log('repsonsei ',response);
       })
     } else {
-      alert('not good')
+      this.formSubmitted = true;
     }
+  }
+
+  duplicateEmailValidator(control: FormControl){
+    let email = control.value;
+    if (email && this.emailIds.includes(email)) {
+      return {
+        duplicateEmailId: {
+          email: email
+        }
+      }
+    }
+    return null;
   }
 
 }
